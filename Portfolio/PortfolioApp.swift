1e703fb28b4f525d8f08302bc73f1315e13bbaec
RushilKothari <rushilkothari25.rk@gmail.com>
2023-12-28 23:17:35 +0530

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Rushil on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
